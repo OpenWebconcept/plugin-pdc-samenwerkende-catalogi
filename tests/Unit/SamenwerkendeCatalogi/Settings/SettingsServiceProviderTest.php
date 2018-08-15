2ac89c02f63dcc3b56f45864ee739419061d601a
Edwin Siebel <edwin@yard.nl>
2018-08-15 15:30:11 +0200

    Run: phpcs + phpcbf

--- tests/Unit/SamenwerkendeCatalogi/Settings/SettingsServiceProviderTest.php
+++ tests/Unit/SamenwerkendeCatalogi/Settings/SettingsServiceProviderTest.php
@@ -12,66 +12,66 @@ use OWC\PDC\SamenwerkendeCatalogi\Tests\Unit\TestCase;
 class SettingsServiceProviderTest extends TestCase
 {
 
-	public function setUp()
-	{
-		\WP_Mock::setUp();
-	}
+    public function setUp()
+    {
+        \WP_Mock::setUp();
+    }
 
-	public function tearDown()
-	{
-		\WP_Mock::tearDown();
-	}
+    public function tearDown()
+    {
+        \WP_Mock::tearDown();
+    }
 
-	/** @test */
-	public function check_registration_of_settings_metaboxes()
-	{
-		$config = m::mock(Config::class);
-		$plugin = m::mock(Plugin::class);
+    /** @test */
+    public function check_registration_of_settings_metaboxes()
+    {
+        $config = m::mock(Config::class);
+        $plugin = m::mock(Plugin::class);
 
-		$plugin->config = $config;
-		$plugin->loader = m::mock(Loader::class);
+        $plugin->config = $config;
+        $plugin->loader = m::mock(Loader::class);
 
-		$service = new SettingsServiceProvider($plugin);
+        $service = new SettingsServiceProvider($plugin);
 
-		$plugin->loader->shouldReceive('addAction')->withArgs([
-			'owc/pdc-base/plugin',
-			$service,
-			'registerSettings',
-			10,
-			1
-		])->once();
+        $plugin->loader->shouldReceive('addAction')->withArgs([
+            'owc/pdc-base/plugin',
+            $service,
+            'registerSettings',
+            10,
+            1
+        ])->once();
 
-		$service->register();
+        $service->register();
 
-		$settings = [
-			'samenwerkende_catalogi' => [
-				'id'             => 'metadata',
-				'settings_pages' => 'base_settings_page',
-				'fields'         => [
-					'general' => [
-						'testfield_noid' => [
-							'type' => 'heading'
-						],
-						'testfield1'     => [
-							'id' => 'metabox_id1'
-						],
-						'testfield2'     => [
-							'id' => 'metabox_id2'
-						]
-					]
-				]
-			]
-		];
+        $settings = [
+            'samenwerkende_catalogi' => [
+                'id'             => 'metadata',
+                'settings_pages' => 'base_settings_page',
+                'fields'         => [
+                    'general' => [
+                        'testfield_noid' => [
+                            'type' => 'heading'
+                        ],
+                        'testfield1'     => [
+                            'id' => 'metabox_id1'
+                        ],
+                        'testfield2'     => [
+                            'id' => 'metabox_id2'
+                        ]
+                    ]
+                ]
+            ]
+        ];
 
-		$config->shouldReceive('get')->with('settings')->once()->andReturn($settings);
+        $config->shouldReceive('get')->with('settings')->once()->andReturn($settings);
 
-		$basePlugin         = new \StdClass();
-		$basePlugin->config = m::mock(Config::class);
+        $basePlugin         = new \StdClass();
+        $basePlugin->config = m::mock(Config::class);
 
-		$basePlugin->config->shouldReceive('set')->withArgs( ['settings.samenwerkende_catalogi', $settings['samenwerkende_catalogi']])->once();
+        $basePlugin->config->shouldReceive('set')->withArgs(['settings.samenwerkende_catalogi', $settings['samenwerkende_catalogi']])->once();
 
-		$this->assertTrue( true );
+        $this->assertTrue(true);
 
-		$service->registerSettings($basePlugin);
-	}
+        $service->registerSettings($basePlugin);
+    }
 }
