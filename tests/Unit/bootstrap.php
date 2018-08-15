27a67db399ad284d13bef24bc74c02dd2ff7587a
Edwin Siebel <edwin@yard.nl>
2018-08-15 15:30:11 +0200

    Run: phpcs + phpcbf

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -14,35 +14,35 @@ define('WP_PLUGIN_DIR', __DIR__);
 /**
  * Bootstrap WordPress Mock.
  */
-\WP_Mock::setUsePatchwork( true );
+\WP_Mock::setUsePatchwork(true);
 \WP_Mock::bootstrap();
 
 $GLOBALS['samenwerkende-catalogi'] = [
-	'active_plugins' => ['samenwerkende-catalogi/samenwerkende-catalogi.php'],
+    'active_plugins' => ['samenwerkende-catalogi/samenwerkende-catalogi.php'],
 ];
 
 class WP_CLI
 {
-	public static function add_command()
-	{
-	}
+    public static function add_command()
+    {
+    }
 }
 
-if ( ! function_exists('get_echo') ) {
+if (! function_exists('get_echo')) {
 
-	/**
-	 * Capture the echo of a callable function.
-	 *
-	 * @param       $callable
-	 * @param array $args
-	 *
-	 * @return string
-	 */
-	function get_echo($callable, $args = [])
-	{
-		ob_start();
-		call_user_func_array($callable, $args);
+    /**
+     * Capture the echo of a callable function.
+     *
+     * @param       $callable
+     * @param array $args
+     *
+     * @return string
+     */
+    function get_echo($callable, $args = [])
+    {
+        ob_start();
+        call_user_func_array($callable, $args);
 
-		return ob_get_clean();
-	}
+        return ob_get_clean();
+    }
 }
